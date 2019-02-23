faf8996d313849e17a10587fe2fac412ff7503be
duyisking <duyisking1060@gmail.com>
2019-02-23 15:41:17 +0700

    Write lint script

---
+++ scripts/lint.js
@@ -0,0 +1,23 @@
+const { CLIEngine } = require('eslint');
+const fs = require('fs');
+const path = require('path');
+const opn = require('opn');
+
+const cli = new CLIEngine({
+    configFile: path.resolve(__dirname, '../configs/.eslintrc.js'),
+    ignorePath: path.resolve(__dirname, '../configs/.eslintignore'),
+    extensions: ['.jsx', '.js'],
+});
+
+const report = cli.executeOnFiles(['src/']);
+const formatter = cli.getFormatter('html');
+const outputPath = path.resolve(__dirname, '../dist/lint-report.html');
+
+fs.writeFile(outputPath, formatter(report.results), (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log(`Lint report is at ${outputPath}.`);
+    opn(outputPath);
+});
