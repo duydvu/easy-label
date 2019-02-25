1b6f804187ecfed7f55a5de795b92b0a432f4b89
duyisking <duyisking1060@gmail.com>
2019-02-25 10:27:30 +0700

    Fix CORS on api server

--- api_server/app.py
+++ api_server/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, Blueprint, request
+from flask_cors import CORS, cross_origin
 from api_server.dataset_manager import DatasetManager
 from bson.json_util import dumps
 
-BLUE_PRINT = Blueprint('easy_label', __name__)
-
+BLUE_PRINT = Blueprint('easy-label', __name__)
+CORS(BLUE_PRINT)
 DATASET_MANAGER = DatasetManager()
 
 
@@ -11,16 +12,18 @@ DATASET_MANAGER = DatasetManager()
 def get_metadata():
     return dumps(DATASET_MANAGER.getMetadata())
 
+
 @BLUE_PRINT.route('/data/<index>', methods=['GET'])
 def get_data(index):
     return dumps(DATASET_MANAGER.findByIndex(int(index)))
+
 
 @BLUE_PRINT.route('/data/<index>', methods=['PUT'])
 def put_data(index):
     document = request.get_json()
     print(document)
-    # return dumps(DATASET_MANAGER.updateByIndex(int(index), document))
-    return '1'
+    return dumps(DATASET_MANAGER.updateByIndex(int(index), document))
+
 
 def create_app():
     app = Flask(__name__)
